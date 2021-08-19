751594510c7149af1f9911f31d92b9635437edda
kmichel1 <kmike411@yahoo.com>
2021-08-18 20:16:27 -0400

    Added a new console.log message.

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -1,11 +1,12 @@
 "use strict";
 console.log("DayTripGenerator");
+console.log("Connected to Github and GitBash");
 
 // As a user, I want a destination to be randomly selected for my day trip.//
 
 function destination(arr){
     let destination =["New York", "Miami", "Tampa", "Atlanta", "Virginia Beach"];
     for(let i = o; i < arr.length; i++){
-
+        
     } 
 }
