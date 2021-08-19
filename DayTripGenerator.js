10567f2f9603e15b7b6fd96b20e2c11ab49a7b16
kmichel1 <kmike411@yahoo.com>
2021-08-18 20:41:25 -0400

    Created a function that randomly selects desitination and print to console.log.

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -1,12 +1,11 @@
 "use strict";
 console.log("DayTripGenerator");
-console.log("Connected to Github and GitBash");
-
 // As a user, I want a destination to be randomly selected for my day trip.//
 
-function destination(arr){
-    let destination =["New York", "Miami", "Tampa", "Atlanta", "Virginia Beach"];
-    for(let i = o; i < arr.length; i++){
-        
-    } 
+function random_destination(items){
+    
+    return items[Math.floor(Math.random() * items.length)];
+    
 }
+let items =["New York", "Miami", "Tampa", "Atlanta", "Virginia Beach"];
+console.log(random_destination(items));
