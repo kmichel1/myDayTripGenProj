c55cfe5ab7758ba92a5e50d4f61314181b7284d9
kmichel1 <kmike411@yahoo.com>
2021-08-18 20:11:53 -0400

    Created files for project 1 and ready for use.

---
+++ DayTripGenerator.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("DayTripGenerator");
+
+// As a user, I want a destination to be randomly selected for my day trip.//
+
+function destination(arr){
+    let destination =["New York", "Miami", "Tampa", "Atlanta", "Virginia Beach"];
+    for(let i = o; i < arr.length; i++){
+
+    } 
+}
